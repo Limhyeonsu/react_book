fdf2f380cfe53b8e48101e9845a4c82391282664
LimHyeonsu <dlagustn720@naver.com>
2022-01-12 20:19:19 +0900

    2. JSX(2)

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,12 +1,19 @@
-import { Fragment } from 'react'
+import "./App.css";
+import { Fragment } from "react";
 
 function App() {
-  const name = '리액트';
-  return(
-      <>
-          <h1>{name} 안녕!</h1>
-      </>
+  const name = "리액트";
+  return (
+    <>
+      {/* 주석 작성 */}
+      <div
+        className="react" //시작 태그 < 를 여러 줄로 작성하면 이 안에 주석 작성 가능
+      >
+        {name}
+      </div>
+      // 주석 아님1 /* 주석 아님2*/
+      <input />
+    </>
   );
 }
-
 export default App;
