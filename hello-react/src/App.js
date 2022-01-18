3e2dad2d59cfa22e22b8081599cb88028fbeb0e5
LimHyeonsu <dlagustn720@naver.com>
2022-01-18 17:14:46 +0900

    4. 이벤트 핸들링

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,7 @@
-import "./App.css";
-import MyComponent from "./MyComponent";
-import Say from "./Say";
+import EventPractice from "./EventPractice";
 
 const App = () => {
-    return <Say />;
+    return <EventPractice />;
 }
 
 export default App;
