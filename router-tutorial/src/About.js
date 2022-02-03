cbb46222f931372f7fbd47e49505e18b85dc17c9
LimHyeonsu <dlagustn720@naver.com>
2022-02-03 17:47:44 +0900

    13. 리액트 라우터 사용(1)

---
+++ router-tutorial/src/About.js
@@ -0,0 +1,20 @@
+import qs from 'qs';
+import {useLocation, useParams} from 'react-router-dom';
+
+const About = ({location}) => {
+  const location1 = useLocation();
+  const query = qs.parse(location1.search, {
+    ignoreQueryPrefix: true //문자열 맨 앞의 ? 생략
+  });
+  //쿼리의 파싱 결과 값은 언제나 문자열임 숫자를 받아와야 하는 경우 parseInt()를 통하여 숫자로 변환해준다.
+  const showDetail = query.detail === 'true';
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>이 프로젝트는 리액트 라우터 기초를 실습해 보는 예제 프로젝트 입니다.</p>
+      {showDetail && <p>detail 값을 true로 설정하셨군요!</p>}
+    </div>
+  );
+};
+
+export default About;
---
+++ router-tutorial/src/App.js
@@ -0,0 +1,33 @@
+import {Route, Routes, Link, MultiRoute} from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+import Profile from './Profile';
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
+      </ul>
+      <hr />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/info" element={<About />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
+    </div>
+  );
+};
+export default App;
