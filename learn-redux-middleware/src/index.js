af8ba4503a4fcf810eae143d05e29048c152359d
LimHyeonsu <dlagustn720@naver.com>
2022-02-17 16:06:44 +0900

    18. 리덕스 미들웨어(1)

---
+++ learn-redux-middleware/src/index.js
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {applyMiddleware, createStore} from 'redux';
+import rootReducer from './modules';
+import {Provider} from 'react-redux';
+import loggerMiddleware from './lib/loggerMiddleware';
+import {createLogger} from 'redux-logger/src';
+
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ learn-redux-middleware/src/lib/loggerMiddleware.js
@@ -0,0 +1,10 @@
+const loggerMiddleware = (store) => (next) => (action) => {
+  console.group(action && action.type); //액션 타입으로 log 그룹화
+  console.log('이전상태', store.getState());
+  console.log('액션', action);
+  next(action); //다음 미들웨어 or 리듀서에게 전달
+  console.log('다음 상태', store.getState()); //업데이트된 상태
+  console.groupEnd();
+};
+
+export default loggerMiddleware;
