9c3357ba8d0543b1b95689d61f2aaa24bb3187d4
LimHyeonsu <dlagustn720@naver.com>
2022-04-11 17:42:22 +0900

    23. JWT를 통한 회원 인증 시스템 구현하기(1) - 회원가입, 로그인

---
+++ blog/blog-backend/src/models/user.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const UserSchema = new Schema({
+  username: String,
+  hashedPassword: String,
+});
+// 인스턴스 메서드 생성, 화살표 함수가 아닌 function 키워드를 사용하여 구현한다. 이유는 this에 접근하기 위해서
+// 비밀번호 설정
+UserSchema.methods.setPassword = async function (password) {
+  const hash = await bcrypt.hash(password, 10);
+  this.hashedPassword = hash;
+};
+// 비밀번호가 일치하는지 검증
+UserSchema.methods.checkPassword = async function (password) {
+  const result = await bcrypt.compare(password, this.hashedPassword);
+  return result;
+};
+
+// 스태틱 메서드 생성
+// 스태틱 메서드 내에서 this는 모델을 가리킨다.
+UserSchema.statics.findByUsername = function (username) {
+  return this.findOne({ username });
+};
+
+UserSchema.methods.serialize = function () {
+  const data = this.toJSON();
+  delete data.hashedPassword;
+  return data;
+};
+
+const User = mongoose.model('User', UserSchema);
+export default User;
